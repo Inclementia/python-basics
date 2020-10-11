031c2750fb8a3e178abeeaba013d24c85da45b6a
Hp <lolitakur72@gmail.com>
2020-10-11 20:32:49 +0500

    lesson 200921

---
+++ 200914/02.lists.py
@@ -0,0 +1,31 @@
+# # data read
+# student_marks = []
+# while True:
+#     mark = input('введите оценку студента:\n')
+#     if mark:
+#         student_marks.append(int(mark))
+#     else:
+#         break
+#
+# print('ввод завершен')
+# print(student_marks)
+
+# mock_student_marks = ['5', '4', '3', '2', '5']  # wrong mock
+mock_student_marks = [5, 4, 3, 2, 5]  # correct mock
+student_marks = mock_student_marks
+
+# data processing
+# i = 0
+# avg_mark = 0
+# while i < len(student_marks):
+#     avg_mark += student_marks[i]
+#     i += 1
+# avg_mark /= len(student_marks)
+# print('средний балл', avg_mark)
+
+avg_mark = 0
+for item in student_marks:
+    avg_mark += item
+
+avg_mark /= len(student_marks)
+print('средний балл', avg_mark)
---
+++ 200914/05.str.py
@@ -0,0 +1,23 @@
+# say = 'всем привет!'
+say = 'вСеМ пРивЕт!'
+print(say)
+# print(dir(say))
+print(say.islower())
+print(say.isupper())
+
+say_2 = say.upper()
+print(say_2)
+
+# say_3 = say_2.capitalize()
+say_3 = say.capitalize()
+print(say_3)
+
+if say.endswith('!'):
+    print('эмоционально сказано')
+
+home_task = '''
+1. Добавить к алгоритму вычисления среднего балла проверку, 
+чтобы оценка была в диапазоне от 1 до 5.
+2. Вывести оценки студентов за конкретную дату.
+3. Изучить методы строк в Python. Написать пару примеров использования.
+PS: файлы с ДЗ в репозитории: hometask_200914_01.py, hometask_200914_02.py
